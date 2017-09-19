e353d54a4e76b10fc877c833112c029511b7d1b0
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-19 17:21:08 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,15 +1,21 @@
 <?
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-use Goutte\Client;
+$client = new GuzzleHttp\Client();
+$res = $client->request('GET', 'https://api.github.com/user', [
+    'auth' => ['user', 'pass']
+]);
+echo $res->getStatusCode();
+// "200"
+echo $res->getHeader('content-type');
+// 'application/json; charset=utf8'
+echo $res->getBody();
+// {"type":"User"...'
 
-
-$client = new Client();
-
-$crawler = $client->request('GET', 'https://www.symfony.com/blog/');
-$link = $crawler->selectLink('Security Advisories')->link();
-$crawler = $client->click($link);
-$crawler->filter('h2 > a')->each(function ($node) {
-    print $node->text()."\n";
+// Send an asynchronous request.
+$request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
+$promise = $client->sendAsync($request)->then(function ($response) {
+    echo 'I completed! ' . $response->getBody();
 });
+$promise->wait();
 ?>
