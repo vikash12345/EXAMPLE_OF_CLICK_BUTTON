4238ee9edc59ef4975a00fef9b8d5e9a7aecd163
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-19 17:18:19 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,15 +1,15 @@
 <?
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-require 'fabpot/goutte';
+composer require fabpot/goutte
 use Goutte\Client;
 
 $client = new Client();
-$crawler = $client->request('GET', 'https://github.com/');
-$crawler = $client->click($crawler->selectLink('Sign in')->link());
-$form = $crawler->selectButton('Sign in')->form();
-$crawler = $client->submit($form, array('login' => 'fabpot', 'password' => 'xxxxxx'));
-$crawler->filter('.flash-error')->each(function ($node) {
-    print $node->text()."\n";
 
+$crawler = $client->request('GET', 'https://www.symfony.com/blog/');
+$link = $crawler->selectLink('Security Advisories')->link();
+$crawler = $client->click($link);
+$crawler->filter('h2 > a')->each(function ($node) {
+    print $node->text()."\n";
+});
 ?>
