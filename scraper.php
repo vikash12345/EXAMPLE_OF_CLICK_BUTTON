627bca60c641f6be2b50df5b69de2455e3c67ee3
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-19 17:14:12 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,7 +5,6 @@ require 'fabpot/goutte';
 use Goutte\Client;
 
 $client = new Client();
-$crawler = $client->request('GET', 'https://www.symfony.com/blog/');
 $crawler = $client->request('GET', 'https://github.com/');
 $crawler = $client->click($crawler->selectLink('Sign in')->link());
 $form = $crawler->selectButton('Sign in')->form();
